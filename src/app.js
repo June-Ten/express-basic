adea6ed4ffe6aefe1213d3ba386df227d03c0b51
qilingwang <891696668@qq.com>
2022-12-28 14:30:15 +0800

    初步预测成功

--- src/app.js
+++ src/app.js
@@ -3,9 +3,20 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import config from '../config/config.js'
 import db from './db/db.js'
+
 import index from './router/index.js'
 import login from './router/login.js'
+import register from "./router/register.js";
+import weatherQuery from "./router/weatherQuery.js";
+import waterNumber from "./router/waterNumber.js";
+import weatherTypeStat from "./router/weatherTypeStat.js";
+import windStat from "./router/windStat.js";
+import predictTemp from "./tensorflow/predictTemp.js";
+
 import windDirection from "./router/windDirection.js";
+import predictMaxTemp from "./tensorflow/predictMaxTemp.js";
+import predictMinTemp from './tensorflow/predictMinTemp.js'
+import test from "./tensorflow/test.js";
 
 const app = express()
 app.use(cors())
@@ -14,7 +25,17 @@ app.use(bodyParser.json())
 db.connect()
 app.use('/',index)
 app.use('/login',login)
+app.use('/register', register)
+app.use('/weatherQuery',weatherQuery)
+app.use('/waterNumber',waterNumber)
+app.use('/weatherTypeStat',weatherTypeStat)
+app.use('/windStat',windStat)
+app.use('/predictTemp',predictTemp)
+
 app.use('/windDirection',windDirection)
+app.use('/tensorflowMaxTemp',predictMaxTemp)
+app.use('/tensorflowMinTemp',predictMinTemp)
+app.use('/test',test)
 
 app.listen(config.port,()=> {
   console.log('serve is running at'+config.port)
--- src/router/login.js
+++ src/router/login.js
@@ -8,12 +8,28 @@ login.post('',(req,res)=> {
   let sql = `select password from user where username="${username}"`
   db.query(sql,(err,results)=> {
     if (!err) {
-      console.log(results)
-      if (password == results[0].password) {
+      if (results.length === 0) {
+        res.send({
+          success: false,
+          msg: '用户名不存在'
+        })
+        return
+      }
+      if (password == results[0]?.password) {
         res.send({
           success: true,
+          username,
+        })
+      } else {
+        res.send({
+          success: false,
+          msg: '密码错误',
         })
       }
+    } else {
+      res.send({
+        success: false,
+      })
     }
   })
 })
